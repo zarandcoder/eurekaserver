f98207e828c5e4db8b3167477a5b8742af29740e
Vadim Zaripov <vadim@zaripov.website>
2020-12-01 02:49:44 +0100

    Initial commit

---
+++ src/test/java/website/zaripov/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package website.zaripov.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
